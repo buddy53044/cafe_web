a02a2f18b1449ba94bdc23a2576f1f5f9ed006be
buddy53044 <buddy53044@gmail.com>
2023-11-02 06:02:23 +0800

    index的全部頁面跳轉

---
+++ js/location.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+
+    var homeLink = $(".nav-text-2");
+    var aboutUsLink = $(".nav-text-4");
+    var menuLink = $(".nav-text");
+    var onlineOrderLink = $(".home-wrapper");
+    var contactUsLink = $(".div-wrapper");
+    var loginLink = $(".nav-text-login");
+    var logoLink = $(".logo");
+    var orderBtnLink = $(".main-btn-2");
+    var menuBtnLink = $(".main-btn");
+    var seemoreBtnLink = $(".two-btn");
+
+
+    homeLink.click(function() {
+        window.location.href = 'index.html'; 
+    });
+
+    aboutUsLink.click(function() {
+        window.location.href = 'about.html'; 
+    });
+
+    menuLink.click(function() {
+        window.location.href = 'menu.html'; 
+    });
+
+    onlineOrderLink.click(function() {
+        window.location.href = 'order.html'; 
+    });
+
+    contactUsLink.click(function() {
+        window.location.href = 'contact.html'; 
+    });
+
+    loginLink.click(function() {
+        window.location.href = 'login.html'; 
+    });
+
+    logoLink.click(function() {
+        window.location.href = 'index.html'; 
+    });
+
+    orderBtnLink.click(function() {
+        window.location.href = 'order.html'; 
+    });
+
+    menuBtnLink.click(function() {
+        window.location.href = 'menu.html'; 
+    });
+
+    seemoreBtnLink.click(function() {
+        window.location.href = 'menu.html'; 
+    });
+
+});
